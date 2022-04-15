d65336c1f9c4f39ab64f54df11f3ab5085e56fe2
rmbrunet <rmbrunet@imcdi.com>
2022-04-15 10:29:00 -0400

    Added some Validation Tests

--- MediatRTestTests/ListCustomersTests.cs
+++ MediatRTestTests/ListCustomersTests.cs
@@ -20,8 +20,25 @@ namespace MediatRTestTests
             mapper = config.CreateMapper();
         }
 
+
         [Fact]
-        public async Task ListCustomersAsync()
+        public async Task ListCustomers_Should_Return_The_Right_Type()
+        {
+            var customerService = CustomerServiceMock.GetCustomerServiceMock(5).Object;
+
+            var handler = new MediatRTest.Features.ListCustomersFeature.QueryHandler(customerService, mapper);
+
+            var query = new MediatRTest.Features.ListCustomersFeature.Query() { Size = 5 };
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Should().NotBeNull();
+            result.Customers.Should().BeAssignableTo<IEnumerable<CustomerDto>>();
+        }
+
+
+        [Fact]
+        public async Task ListCustomers_Should_Return_The_Right_Numbers_Of_Items()
         {
             var customerService = CustomerServiceMock.GetCustomerServiceMock(5).Object;
 
@@ -33,7 +50,36 @@ namespace MediatRTestTests
 
             result.Should().NotBeNull();
             result.Customers.Count().Should().Be(5);
-            result.Customers.Should().BeAssignableTo<IEnumerable<CustomerDto>>();
         }
+
+        [Fact]
+        public void ListCustomers_Should_Accept_Valid_Query()
+        {
+            var query = new MediatRTest.Features.ListCustomersFeature.Query() { Size = 5 };
+
+            var validator = new MediatRTest.Features.ListCustomersFeature.ModelValidator();
+
+
+            var result = validator.Validate(query);
+
+            result.IsValid.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void ListCustomers_Should_Not_Accept_Invalid_Query(int size)
+        {
+            var query = new MediatRTest.Features.ListCustomersFeature.Query() { Size = size };
+
+            var validator = new MediatRTest.Features.ListCustomersFeature.ModelValidator();
+
+
+            var result = validator.Validate(query);
+
+            result.IsValid.Should().Be(false);
+        }
+
+
     }
 }
